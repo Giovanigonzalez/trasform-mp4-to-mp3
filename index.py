705311cef4fc64519ac41512f1f1b56e268b1645
Giovanigonzalez <villalbafernando319@gmail.com>
2022-09-10 23:55:54 -0400

    primer commit

---
+++ index.py
@@ -0,0 +1,10 @@
+import moviepy
+
+import moviepy.editor
+
+"""
+INGRESE TU ARCHIVO DE MP4 ENTRE LAS COMILLAS
+"""
+video=moviepy.editor.VideoFileClip("")
+audio=video.audio
+audi.write_audiofile("nuevo_audio.mp3")
